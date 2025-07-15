0975951e2ab0b2ef669a75a1e369e9088af4c102
pawankriplani <pawankriplani604@gmail.com>
2025-07-15 18:17:12 +0530

    Initial commit

---
+++ src/main/java/com/example/interview_hub/InterviewHubApplication.java
@@ -0,0 +1,12 @@
+package com.example.interview_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewHubApplication.class, args);
+    }
+}
